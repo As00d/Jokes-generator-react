c63222902a69a0a735029e262a4bcf759929c62f
Ankita Sood <ankisood@amazon.com>
2024-08-26 09:00:04 +0530

    Jokes generator app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [joke, setJoke] = useState("");
+  const getJoke = async function () {
+    const response = await fetch(
+      "https://sv443.net/jokeapi/v2/joke/Programming?type=single"
+    );
+    const output = await response.json();
+    setJoke(output.joke);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1 className="title">Joke Generator Using React and Joke API</h1>
+        <button onClick={getJoke}>Click to generate a joke.</button>
+        <h3>{joke}</h3>
+      </div>
     </div>
   );
 }
